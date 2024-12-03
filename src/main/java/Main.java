8dafa33351abb0bb8de3baeb2de63472162d3363
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-03 13:42:20 -0600

    Variables, data types, operators and clean code practices

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello, JavaBank ATM!");
+        // Basic variables
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+        int pin = 1234;
+
+        // Transaction amounts Array
+        int[] transactionAmounts = {200, -100, 50};
+
+        // Operations with variables
+        balance += transactionAmounts[0]; // Deposit
+        if(balance > 0 && pin == 1234) {
+            System.out.println("Access granted");
+        }
+
+        // Operators usage
+        balance++; // Increment
+        String status = (balance < 0) ? "Debt" : "Credit";
+        System.out.println("Account Status: " + status);
     }
 }
